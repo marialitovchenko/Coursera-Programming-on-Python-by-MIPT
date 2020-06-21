961f4b0bdbae4218423c5653c5778bc58cdb0479
Maria Litovchenko <maria.litovchenko@gmail.com>
2020-06-21 17:24:21 +0200

    Updated file description

--- 1_DiveIn_Python/Week_3/FileReader/solution.py
+++ 1_DiveIn_Python/Week_3/FileReader/solution.py
@@ -3,9 +3,9 @@ Write python module solution.py containing class FileReader. The constructor
 should accept 1 parameter: file to the path. There should be methods read,
 which would return a string with the line from the file.
 
-Python модуль должен быть написан таким образом, чтобы импорт класса FileReader из него не вызвал ошибок.
+Import of this file like module shouldn't call errors.
 
-При написании реализации метода read, вам нужно учитывать случай, когда при инициализации был передан путь к несуществующему файлу. Требуется обработать возникающее при этом исключение FileNotFoundError и вернуть из метода read пустую строку.
+Read method should handle FileNotFoundError
 """
 
 
